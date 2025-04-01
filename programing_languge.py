d0961fce4dc3110660ac14e5ba8667ce2a6ead45
Eugen00056 <eugen.somin@gmail.com>
2025-04-01 21:43:54 +0300

    Update programing_languge.py

--- programing_languge.py
+++ programing_languge.py
@@ -1,38 +1,17 @@
+from PIL import Image
 from tkinter import *
 from tr import *
 from tkinter import ttk
+import tkinter as tk
 root = Tk()
 root.title("...") 
-root.geometry("1202x700")  
-label = Label(text="")
-label3 = Label(text="") 
-label.place(x=340,y=0)
-label4 = Label(text="")
-label4.place(x=940,y=601)
-label4 = Label(text="")
-label4.place(x=940,y=501)
-label5 = Label(text="Часть окна. здесь нужно построчно писать \nкод на питоне для вызова ошибки и написания\nглавного кода на Error?Error!(этот язык)")
-label5.place(x=600,y=521)
-label3.place(x=0,y=20)
-comand = ''
-entry = ttk.Entry()
-entry.place(x=740,y=590)
-entry2 = ttk.Entry()
-entry2.place(x=50,y=650)
-code = ''
-c = 0
-def delete():
-    global code
-    code = ""
-    label4.config(text = code)
-def add():
-    global code
-    code += entry.get()+'\n'
-    label4.config(text = code)
+root.geometry("1250x700")  
 def ask():
     global comand
     try:
+        code = text_input.get("1.0", "end-1c") # type: ignore
         exec(code)
+        print('Плохой код. Нет ошибок')
     except TypeError:
         comand += '  TypeError'
     except NameError:
@@ -59,51 +38,70 @@ def ask():
         comand += '  RecursionError'
     except UnicodeEncodeError:
         comand += '  UnicodeEncodeError'
-    label.config(text = comand)
+    except:
+        print('нипонятая ошибка')
+    Main_code.config(text = comand) # type: ignore
     if len(comand.split('\n')[-1]) >120:
         comand += '\n'
-def add2():
-    comand += f'  {entry.get()}'
+def add():
+    global comand
+    comand += f'  {str(adding_ord.get())[0]}' # type: ignore
+    Main_code.config(text = comand) # type: ignore
     if len(comand.split('\n')[-1]) >120:
         comand += '\n'
-
-def help():
-    global c
-    if c == 0:
-        label3.config(text = 'NameError: "."\nIndexError: ","\nTypeError: "<"\nKeyError: ">"\nZeroDivisionError: "-"\nSyntaxError: "+"\nValueError: "["\nStopIteration: "]"\nEOFError: переход на первую ячейку\nMemoryError: в текущую ячейку будет\n записан номер первого\n вхождения ord символа идущего\n после этой команды \nсреди ячеек\nUnicodeEncodeError: позволяет записать отдельный кусочек \nкода в специальную ячейку, закрывается "RecursionError"\nSystemExit: выполняет сохраненный кусочек кода')
-        c=1
-    else:
-        label3.config(text = '')
-        c=0
 def plus_to_command1(text):
     global comand
     if text=='Backspace':
         comand = comand[:len(comand) - len(comand.split('  ')[-1])-2]
-        label.config(text = comand)
+        Main_code.config(text = comand) # type: ignore
 def delete_all():
     global comand
     comand = ''
-    label.config(text = comand)
+    Main_code.config(text = comand) # type: ignore
 def go():
     global comand
     txt = run(comand)
-    label2.config(text = f'Результат:\n{txt}')
-button1= ttk.Button(text="Backspace", command= lambda:  plus_to_command1('Backspace'))
-button2= ttk.Button(text="Run", command= go)
-button3= ttk.Button(text="help", command= help)
-button4= ttk.Button(text="delete all", command= delete_all)
-button5= ttk.Button(text="run for find an error", command= ask)
-button6= ttk.Button(text="add to code", command= add)
-button7= ttk.Button(text="add to main code", command= add2)
-button8= ttk.Button(text="del code", command= delete)
-label2 = Label(text=f"Результат:")
-button1.place(x=450,y=650)
-button2.place(x=350,y=650)
-button3.place(x=0,y=0)
-button4.place(x=260,y=650)
-button5.place(x=790,y=650)
-button6.place(x=700,y=650)
-button8.place(x=700,y=620)
-button7.place(x=150,y=650)
-label2.place(x=380,y=470)
+    if len(txt)> 23:
+        txt_2 = ''
+        for i in range(0,len(txt),23):
+            txt_2 += txt[i:i+23]+ '\n'
+        txt = txt_2
+
+    result.config(text = f'Результат:\n{txt}') # type: ignore
+def Made_all():
+    Main_code = tk.Label(root, text="",justify=tk.LEFT)
+    label3 = tk.Label(root, text="",justify=tk.LEFT)
+    Main_code.place(x=340,y=0)
+    label4 = tk.Label(root, text="",justify=tk.LEFT)
+    label4.place(x=940,y=601)
+    info = tk.Label(root, text="",justify=tk.LEFT)
+    info.place(x=0,y=0)
+    info.config(text = 'NameError: "."\nIndexError: ","\nTypeError: "<"\nKeyError: ">"\nZeroDivisionError: "-"\nSyntaxError: "+"\nValueError: "["\nStopIteration: "]"\nEOFError: переход на первую ячейку\nMemoryError: в текущую ячейку будет записан\nномер первого вхождения ord символа идущего\nпосле этой команды среди ячеек\nUnicodeEncodeError: позволяет записать отдельный\nкусочек кода в специальную ячейку, закрывается\n"RecursionError"\nSystemExit: выполняет сохраненный кусочек кода')
+    comand = ''
+    adding_ord = ttk.Entry()
+    adding_ord.place(x=0,y=650)
+    code = ''
+    entry = tk.Frame(root)
+    entry.place(x=750,y=460)
+    scrollbar = tk.Scrollbar(entry)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    text_input = tk.Text(entry, 
+                        height=10, 
+                        width=50, 
+                        yscrollcommand=scrollbar.set)
+    text_input.pack(side=tk.LEFT)
+    scrollbar.config(command=text_input.yview)
+    Backspace_button= ttk.Button(text="Backspace", command= lambda:  plus_to_command1('Backspace'))
+    Run_button= ttk.Button(text="Run", command= go)
+    delete_all_button= ttk.Button(text="delete all", command= delete_all)
+    run_for_find_an_error_button= ttk.Button(text="run for find an error", command= ask)
+    add_to_main_code_one_symbol= ttk.Button(text="add to main code one symbol\nfor (MemoryError)", command= add)
+    Backspace_button.place(x=520,y=650)
+    Run_button.place(x=430,y=650)
+    delete_all_button.place(x=340,y=650)
+    run_for_find_an_error_button.place(x=850,y=650)
+    add_to_main_code_one_symbol.place(x=150,y=650)
+    result = tk.Label(root, text="Результат:",justify=tk.LEFT)
+    result.place(x=500,y=470)
+Made_all()
 root.mainloop()
